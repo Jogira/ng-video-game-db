6aadff9594f0e10419db1328897e662dc0fbef70
Jonathan <Zweitonegoismus@gmail.com>
2023-04-28 13:29:27 -0400

    SCSS and navbar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,18 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './components/home/home.component';
 
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -24,6 +29,7 @@ import { MatSelectModule } from '@angular/material/select';
     BrowserAnimationsModule,
     GaugeModule.forRoot(),
     MatFormFieldModule,
+    FormsModule,
     MatSelectModule,
     MatTabsModule,
     MatIconModule
