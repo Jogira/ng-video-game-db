ec4240f97ca90a32e27ef0253f56964e697084c0
Jonathan <Zweitonegoismus@gmail.com>
2023-05-15 12:05:01 -0400

    Genned game-tabs component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { HTTP_INTERCEPTORS, HttpClientModule, HttpHeaderResponse } from '@angula
 import { HttpHeadersInterecptor } from './interceptors/http-headers.interceptor';
 import { HttpErrorsInterceptor } from './interceptors/http-errors.interceptor';
 import { DetailsComponent } from './components/details/details.component';
+import { GameTabsComponent } from './components/game-tabs/game-tabs.component';
 
 
 
@@ -26,7 +27,8 @@ import { DetailsComponent } from './components/details/details.component';
     AppComponent,
     SearchBarComponent,
     HomeComponent,
-    DetailsComponent
+    DetailsComponent,
+    GameTabsComponent
   ],
   imports: [
     BrowserModule,
