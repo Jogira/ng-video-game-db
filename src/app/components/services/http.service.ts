5882bf8d73c160209206b30422aee8c725138337
Jonathan <Zweitonegoismus@gmail.com>
2023-05-16 14:55:41 -0400

    Game tabs SCSS.

--- src/app/components/services/http.service.ts
+++ src/app/components/services/http.service.ts
@@ -14,9 +14,8 @@ export class HttpService {
 
   getGameDetails(id: string): Observable<Game> {
     const gameInfoRequest = this.http.get(`${env.BASE_URL}/games/${id}`);
-    // const gameTrailerRequest = this.http.get(`${env.BASE_URL}/games/${id}/movies`);
-    // const gameScreenshotsRequests = this.http.get(`${env.BASE_URL}/games/${id}/screenshots`);
-    console.log("LOOK HERE");
+    const gameTrailerRequest = this.http.get(`${env.BASE_URL}/games/${id}/movies`);
+    const gameScreenshotsRequests = this.http.get(`${env.BASE_URL}/games/${id}/screenshots`);
 
 
     return forkJoin({
